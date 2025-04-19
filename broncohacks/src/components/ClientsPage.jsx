7aefc3836ed3702dda74768654c6790ab2ca3c1d
Jonathan Woline <jonwoline@gmail.com>
2025-04-19 11:37:47 -0700

    added icons

--- broncohacks/src/components/ClientsPage.jsx
+++ broncohacks/src/components/ClientsPage.jsx
@@ -7,7 +7,14 @@ function ClientItem(props) {
                 <div className="text-left text-2xl m-3">View Clients</div>
             </div>
             <ClientsList></ClientsList>
-            <div>Add Client Section</div>
+            <div className="border-3 rounded-sm m-1 p-3 border-secondary-green">
+                <div className="text-left text-2xl mb-2">Connect With a Client</div>
+                <form className="flex flex-col space-y-4">
+                    <input className="bg-healthygreen rounded-sm p-1" type="text" placeholder="Client Name..."></input>
+                    <input className="bg-healthygreen rounded-sm p-1"type="text" placeholder="Client Code..."></input>
+                    <button className="bg-secondary-green p-1" type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
--- broncohacks/src/components/Navbar.jsx
+++ broncohacks/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 function Navbar(props) {
     return (
     <>
-        <div className={`flex flex-col space-y-8 text-xl w-fit h-full bg-healthygreen p-3`}>
+        <div className={`flex flex-col justify-between w-40 text-xl h-full bg-healthygreen p-3`}>
+            <a href="/" className="flex flex-row justify-between items-center text-md text-end bg-offwhite w-full p-1 rounded-sm">
+                <img className="h-6" src="assets/home-icon.png"></img>
+                <span>Home</span>
+            </a>
             <div>
-                <a href="/" className="bg-secondary-green p-1 rounded-sm">Home</a>
-            </div>
-            <div className="">
-                <a href="/logout" className="bg-secondary-green p-1 rounded-sm">Logout</a>
+                <a href="/logout" className="flex flex-row justify-between items-center text-md text-end bg-offwhite w-full p-1 rounded-sm">
+                    <img className="h-6" src="assets/logout-icon.png"></img>
+                    <span>Logout</span>
+                </a>
             </div>
         </div>
     </>
